2289e746205bafad913de027f2718108d6b152a8
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-11-20 18:14:48 -0500

    feat: :sparkles: Ejercicio 6

---
+++ ejercicios_herencia/src/main/java/com/sofia/Ejercicio6/Bird.java
@@ -0,0 +1,8 @@
+package com.sofia.Ejercicio6;
+
+public class Bird {
+
+    public void fly() {
+        System.out.println("El pajaro esta volando");
+    }
+}
---
+++ ejercicios_herencia/src/main/java/com/sofia/Ejercicio6/Eagle.java
@@ -0,0 +1,11 @@
+package com.sofia.Ejercicio6;
+
+public class Eagle extends Bird {
+    
+    @Override
+    public void fly() {
+        super.fly(); 
+        System.out.println("El aguila vuela mas alto");
+        
+    }
+}
